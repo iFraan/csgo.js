8c7a55dc3a4e2b327c4d9dc707f16282807152b3
Francisco Zorat <franciscozorat@gmail.com>
2022-12-06 09:56:04 -0300

    test command

--- test.js
+++ test.js
@@ -6,8 +6,11 @@ m = (async () => {
     try {
         const user = await API.fetchUser('iFraan_', STEAM_KEY);
         const maps = user.maps()
+        console.log('Maps: ', maps);
         const stats = user.stats()
-        console.log(stats)
+        console.log('Stats: ', stats);
+        const weapons = user.weapons()
+        console.log('Weapons: ', weapons);
     } catch (e) {
         console.log(e)
     }
