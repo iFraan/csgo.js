8d00dcd920bb88b02aec2ea117b739ada53d6be6
Francisco Zorat <franciscozorat@gmail.com>
2024-01-28 02:15:53 -0300

    chore: fix path in test

--- test.ts
+++ test.ts
@@ -1,28 +1,26 @@
-import { API } from './index';
+import { API } from './src/index';
 const STEAM_KEY = '';
 
 const test = async () => {
-
-    try {
-        const user = await API.fetchUser('iFraan_', STEAM_KEY);
-        const info = user.info();
-        console.log('Info', info)
-        const maps = user.maps()
-        console.log('Maps: ', maps);
-        const stats = user.stats()
-        console.log('Stats: ', stats);
-        const weapons = user.weapons()
-        console.log('Weapons: ', weapons);
-        const lastMatch = user.lastMatch()
-        console.log('lastMatch: ', lastMatch);
-        const unknown = user.unknown()
-        console.log('unknown: ', unknown);
-        // const raw = user.raw;
-        // console.log('raw: ', raw);
-    } catch (e) {
-        console.log(e)
-    }
-
+  try {
+    const user = await API.fetchUser('iFraan_', STEAM_KEY);
+    const info = user.info();
+    console.log('Info', info);
+    const maps = user.maps();
+    console.log('Maps: ', maps);
+    const stats = user.stats();
+    console.log('Stats: ', stats);
+    const weapons = user.weapons();
+    console.log('Weapons: ', weapons);
+    const lastMatch = user.lastMatch();
+    console.log('lastMatch: ', lastMatch);
+    const unknown = user.unknown();
+    console.log('unknown: ', unknown);
+    // const raw = user.raw;
+    // console.log('raw: ', raw);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 test();
